4e3cb9d263e15f4f586a96ddfce6be8cb1e1300d
ezkielkibiego <kibiezekiel@gmail.com>
2026-03-25 10:49:11 +0300

    feat: add Fruit model with schema for name, price, and quantity

---
+++ models/Fruit.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const fruitSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+export default mongoose.model("Fruit", fruitSchema);
