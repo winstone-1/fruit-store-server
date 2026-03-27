c7a578e9bb5111e5fc4ff598ddf9d3bf36bc0cf8
winstone-1 <winstonemuna404@gmail.com>
2026-03-27 11:47:14 +0300

    Allow CORS support with allowed origins for api requests

--- index.js
+++ index.js
@@ -3,11 +3,31 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import fruitRoutes from './routes/fruitRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import cors from 'cors'
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5173',
+  'https://fruit-store-client.vercel.app'
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+
+}));
 connectDB();
 
 const PORT = process.env.PORT || 3000;
