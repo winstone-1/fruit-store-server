9aaa5bc58323b50c8df76ce5389432afc2fcb283
winstone-1 <winstonemuna404@gmail.com>
2026-03-27 10:51:58 +0300

    Add Vercel.json file

--- middleware/protect.js
+++ middleware/protect.js
@@ -19,3 +19,11 @@ export const protect = async (req, res, next) => {
         res.status(401).json({ error: 'Not authorized to access this route, Invalid token' });
     }
 };  
+
+export const adminOnly = (req, res, next) => {
+    if (req.user && req.user.role === 'admin') {
+        next();
+    } else {
+        res.status(403).json({ error: 'Admin access only' });
+    }
+};
